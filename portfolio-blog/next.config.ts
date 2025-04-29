343762f0f24cb06ab57f83bfdc0a498a95e1facd
Kim-kyuho <kgh9002@icloud.com>
2025-04-30 00:53:57 +0900

    add source

--- portfolio-blog/next.config.ts
+++ portfolio-blog/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // 정적 사이트로 내보내기 설정
+  trailingSlash: true, // URL 끝에 슬래시 추가
 };
 
 export default nextConfig;
