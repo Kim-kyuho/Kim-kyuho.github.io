dc77fb873d63462843adc9f4cb37a48458637076
Kim-kyuho <kgh9002@icloud.com>
2025-05-01 01:30:44 +0900

    build

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // 정적 사이트로 내보내기 설정
   trailingSlash: true, // URL 끝에 슬래시 추가
-  assetPrefix: isProd ? '/portfolio-blog/' : '',
-  basePath: isProd ? '/portfolio-blog' : '',
 };
 
 export default nextConfig;
