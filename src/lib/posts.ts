34a236756073ac46858d815ce3ccb050629bb78c
Kim-kyuho <kgh9002@icloud.com>
2025-05-01 20:45:27 +0900

    Commit blog

---
+++ src/lib/posts.ts
@@ -0,0 +1,25 @@
+// src/lib/posts.ts
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getAllPosts() {
+  const filenames = fs.readdirSync(postsDirectory);
+
+  return filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+
+    const { data, content } = matter(fileContents);
+
+    const slug = filename.replace(/\.md$/, "");
+
+    return {
+      slug,
+      content,
+      ...data,
+    };
+  });
+}
