be985aa67185c20c0ed252f8e648ddb67242e272
Kim-kyuho <kgh9002@icloud.com>
2025-05-03 23:53:58 +0900

    Update Blog Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center p-4 sm:px-8 border-b border-gray-200 dark:border-gray-700">
       <h1 className="text-xl font-bold text-foreground">
-        <Link href="/" className="text-xl font-bold transition duration-300 hover:scale-105 hover:text-sky-500 font-mono">•Console.log(&apos;Kyuho&apos;)</Link>
+        <Link href="/" className="text-xl font-bold transition duration-300 hover:scale-105 hover:text-sky-500 font-mono">•kyu.log</Link>
       </h1>
       <nav className="space-x-4 text-sm sm:text-base">
         <Link href="/about" className="hover:underline">About</Link>
