955324fc42f6486718ab3d8315447eca6e3ef3cc
Kim-kyuho <kgh9002@icloud.com>
2025-05-04 12:24:23 +0900

    Update Blog Page

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";// 원하는 테마로 바꿔도 OK
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
     <div className="prose dark:prose-invert">
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeHighlight, rehypeSlug, rehypeAutolinkHeadings]}
+      >
         {content}
       </ReactMarkdown>
     </div>
