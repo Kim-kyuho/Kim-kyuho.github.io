493d386b7c428c408d7e9a0671e5a8ad9d8c5604
Kim-kyuho <kgh9002@icloud.com>
2025-05-06 15:35:40 +0900

    Add Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Providers>
           <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
             <Header />
-            <main className="p-4 sm:p-8">{children}</main>
+            <main className="p-4 sm:p-8 overflow-x-hidden">{children}</main>
             <Footer />
           </div>
         </Providers>
