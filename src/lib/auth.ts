f05627ca4ce6692e8e56405c002f3d023f7085d4
Kim-kyuho <kgh9002@icloud.com>
2025-05-06 11:45:58 +0900

    UPdate UI

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import GitHubProvider from "next-auth/providers/github";
 export const authOptions: NextAuthOptions = {
     providers: [
         GitHubProvider({
-                clientId: process.env.GITHUB_ID!,
+                clientId: process.env.REPO_OWNER!,
                 clientSecret: process.env.GITHUB_SECRET!,
             profile(profile) {
                 return {
