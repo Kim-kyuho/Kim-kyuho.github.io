5fc197312214950f325031f95afba0ee468832c9
Kim-kyuho <kgh9002@icloud.com>
2025-06-01 21:14:14 +0900

    Add favicon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@ export default function About() {
   return (
     <div className="max-w-3xl mx-auto px-4 py-8"> {/* Optional container centering */}
       {/* Introduction Section (Top Card) */}
-      <div className="bg-white hover:bg-sky-200 shadow-xl transition duration-300 hover:shadow-sky-200/50 dark:bg-gray-800 rounded-xl p-6 mb-8">
-        <p className="text-lg leading-7 text-blue-950 dark:text-gray-100 dark:hover:text-sky-950 transition duration-300 mb-1">
+      <div className="bg-white hover:bg-sky-200 shadow-xl transition duration-300 hover:shadow-sky-200/50 dark:bg-gray-800 dark:hover:text-sky-950 rounded-xl p-6 mb-8">
+        <p className="text-lg leading-7 text-blue-950 dark:text-gray-100 transition duration-300 mb-1">
           Webエンジニアの**KYU**と申します。Web開発を通じて使いやすく、
           価値のあるサービスを作ることにやりがいを感じており、常に学び続ける姿勢を大切にしています。
         <br/><br/>
@@ -14,13 +14,13 @@ export default function About() {
       </div>
 
       {/* Qualifications & Skills Section (Bottom Card) */}
-      <div className="bg-white hover:bg-pink-200 shadow-xl hover:shadow-pink-200/50 transition duration-300 dark:bg-gray-800 rounded-xl p-3 mb-8">
-        <p className="text-lg leading-7 text-red-950 dark:text-gray-100 dark:hover:text-pink-950 transition duration-300 mb-1">
+      <div className="bg-white hover:bg-pink-200 shadow-xl hover:shadow-pink-200/50 transition duration-300 dark:bg-gray-800 dark:hover:text-pink-950 rounded-xl p-3 mb-8">
+        <p className="text-lg leading-7 text-red-950 dark:text-gray-100 transition duration-300 mb-1">
           <strong>保有資格:</strong> 日本語能力試験N1、(韓国)情報処理技師資格
         </p>
       </div>
-      <div className ="bg-white hover:bg-emerald-200 shadow-xl hover:shadow-emerald-200/50 transition duration-300 dark:bg-gray-800 rounded-xl p-3">
-        <p className="text-lg font-bold text-yellow-950 dark:text-gray-100 dark:hover:text-emerald-950 transition duration-300 mb-2">
+      <div className ="bg-white hover:bg-emerald-200 shadow-xl hover:shadow-emerald-200/50 transition duration-300 dark:bg-gray-800 dark:hover:text-emerald-950 rounded-xl p-3">
+        <p className="text-lg font-bold text-yellow-950 dark:text-gray-100  transition duration-300 mb-2">
           使用スキル:
         </p>
         <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
